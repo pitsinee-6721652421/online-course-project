aa5fd75da2694c83ebbb6fd50f678f78af025f34
pitsinee-6721652421 <pitsinee.ka@ku.th>
2026-03-15 17:50:40 +0700

    add.

---
+++ frontend/study/course3.js
@@ -0,0 +1,29 @@
+let progress = 0;
+
+function finishLesson(){
+
+progress = 100;
+
+document.getElementById("progress").style.width = progress + "%";
+
+alert("คุณเรียนจบบทนี้แล้ว!");
+}
+
+
+function checkQuiz(){
+
+let answer = document.querySelector('input[name="q1"]:checked');
+
+if(!answer){
+alert("กรุณาเลือกคำตอบ");
+return;
+}
+
+if(answer.value === "b"){
+document.getElementById("result").innerHTML = "✅ ถูกต้อง! <br>จบบทเรียนแล้ว!👍🎉";
+
+}else{
+document.getElementById("result").innerHTML = "❌ ตอบผิด";
+}
+
+}
